63fd45ac01217d005a99ee123129a780bc35d825
xvitkov <84027433+xvitkov@users.noreply.github.com>
2021-05-11 17:18:18 +0200

    Add files via upload

--- bad_pickle.py
+++ bad_pickle.py
@@ -2,22 +2,20 @@
 # taken from https://hackernoon.com/10-common-security-gotchas-in-python
 # -and-how-to-avoid-them-e19fbe265e03
 
-
-
-# Input injection
 def transcode_file(filename):
+    """Input injection"""
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     return command
     # a bad idea!
 
 
-# Assert statements
 def access_function(user):
+    """Assert statements"""
     assert user.is_admin, 'user does not have access'
     # secure code...
 
 
-# Pickles
 class RunBinSh():
+    """Pickles"""
     def __reduce__(self):
         return
